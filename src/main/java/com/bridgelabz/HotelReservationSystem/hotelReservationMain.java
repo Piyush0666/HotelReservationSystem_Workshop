152b8b0b71b5cee6c5299ed20f826c3fa22cf112
your_github_username <your_github_email>
2022-03-07 08:26:27 +0530

    Welcome_Message:Hotel_Reservation_System

---
+++ src/main/java/com/bridgelabz/HotelReservationSystem/hotelReservationMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.HotelReservationSystem;
+
+public class hotelReservationMain
+{
+    public static void main(String[] args) {
+        System.out.println("Hotel Reservation System");
+    }
+}
