fde98e8c9d8709b4dca80c04cc0b4177622fee2f
your_github_username <your_github_email>
2022-03-07 09:50:59 +0530

    Ability_to_add_hotel

---
+++ src/main/java/com/bridgelabz/HotelReservationSystem/hotelReservationIF.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.HotelReservationSystem;
+
+public interface hotelReservationIF
+{
+    public void addHotel();
+    public int getHotelListSize();
+    public void printHotelList();
+}
