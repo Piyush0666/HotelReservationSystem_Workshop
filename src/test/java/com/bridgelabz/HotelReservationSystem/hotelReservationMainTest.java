0394a133accb12f281c85ec34b4ba143248f8ccd
your_github_username <your_github_email>
2022-03-07 08:26:16 +0530

    Welcome_Message:Hotel_Reservation_System

---
+++ src/test/java/com/bridgelabz/HotelReservationSystem/hotelReservationMainTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.HotelReservationSystem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class hotelReservationMainTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
