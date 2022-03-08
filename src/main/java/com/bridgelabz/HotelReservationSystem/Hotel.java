8cf64fa9f239ede2eb2c18ff32581c9ddda293b3
your_github_username <your_github_email>
2022-03-08 16:22:44 +0530

    test_case_for_cheapest_hotel_in_given_date_range

--- src/main/java/com/bridgelabz/HotelReservationSystem/Hotel.java
+++ src/main/java/com/bridgelabz/HotelReservationSystem/Hotel.java
@@ -2,48 +2,57 @@ package com.bridgelabz.HotelReservationSystem;
 
 public class Hotel
 {
-    private String hotelName;
-    private int rate;
-    private double weekDayRate;
-    private double weekendRate;
-    public Hotel() {
-    }
+        private String hotelName;
+        private int rating;
+        private double weekDayRate;
+        private double weekendRate;
+        public Hotel() {
+        }
 
-    public String getHotelName() {
-        return hotelName;
-    }
+        public Hotel(String hotelName, int rating, double weekDayRate, double weekendRate) {
+            this.hotelName = hotelName;
+            this.rating = rating;
+            this.weekDayRate = weekDayRate;
+            this.weekendRate=weekendRate;
+        }
 
-    public void setHotelName(String hotelName) {
-        this.hotelName = hotelName;
-    }
+        public String getHotelName() {
+            return hotelName;
+        }
 
-    public int getRate() {
-        return rate;
-    }
+        public void setHotelName(String hotelName) {
+            this.hotelName = hotelName;
+        }
 
-    public void setRate(int rate) {
-        this.rate = rate;
-    }
+        public int getRating() {
+            return rating;
+        }
 
-    public double getWeekDayRate() {
-        return weekDayRate;
-    }
+        public void setRating(int rating) {
+            this.rating = rating;
+        }
 
-    public void setWeekDayRate(double weekDayRate) {
-        this.weekDayRate = weekDayRate;
-    }
+        public double getWeekDayRate() {
+            return weekDayRate;
+        }
 
-    public double getWeekendRate() {
-        return weekendRate;
-    }
+        public void setWeekDayRate(double weekDayRate) {
+            this.weekDayRate = weekDayRate;
+        }
 
-    public void setWeekendRate(double weekendRate) {
-        this.weekendRate = weekendRate;
-    }
+        public double getWeekendRate() {
+            return weekendRate;
+        }
 
-    @Override
-    public String toString() {
-        return "Hotel [hotelName=" + hotelName + ", rating=" + rate + ", weekDayRate=" + weekDayRate
-                + ", weekendRate=" + weekendRate + "]";
+        public void setWeekendRate(double weekendRate) {
+            this.weekendRate = weekendRate;
+        }
+
+        @Override
+        public String toString() {
+            return "Hotel [hotelName=" + hotelName + ", rating=" + rating + ", weekDayRate=" + weekDayRate
+                    + ", weekendRate=" + weekendRate + "]";
+        }
+
+
     }
-}
